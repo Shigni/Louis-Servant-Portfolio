c3b92d533ddd0799b1b86d9dca09210d850e09d7
shigni <louisservant86@gmail.com>
2023-06-16 15:12:09 +0200

    Created burger menu for small screens

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,20 +1,66 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import cross from "../../assets/cross.svg";
+import burger from "../../assets/burger.svg";
 
 export function Header() {
   const title = "Louis.";
+
+  const [background__class, setBackgroundClass] = useState("background_class");
+  const [burger__img, setBurgerImg] = useState(burger);
+  const [button__class, setButtonClass] = useState("button_class");
+  const [nav__links, setNavLinksClass] = useState("nav_links");
+  const [nav__link, setNavLinkClass] = useState("nav_link");
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const updateMenu = () => {
+    if (!isMenuActive) {
+      setBurgerImg(cross);
+      setButtonClass("button_class button_class-active");
+      setNavLinksClass("nav_links nav_links-active");
+      setNavLinkClass("nav_link nav_link-active");
+      setBackgroundClass("background_class background_class-active");
+    } else {
+      setBurgerImg(burger);
+      setButtonClass("button_class");
+      setNavLinksClass("nav_links");
+      setNavLinkClass("nav_link");
+      setBackgroundClass("nav_links");
+    }
+    setIsMenuActive(!isMenuActive);
+  };
+
   return (
     <nav>
       <NavLink activeclassname="active" className="logo-nav-link" to="/">
         <h1 className="logo">{title}</h1>
       </NavLink>
-      <div className="nav-links">
-        <NavLink activeclassname="active" className="nav-link" to="/">
+      <div className={background__class}></div>
+      <button className={button__class} onClick={updateMenu}>
+        <img src={burger__img} alt="Menu burger" />
+      </button>
+      <div className={nav__links}>
+        <NavLink
+          activeclassname="active"
+          className={nav__link}
+          to="/"
+          onClick={updateMenu}
+        >
           Home
         </NavLink>
-        <NavLink activeclassname="active" className="nav-link" to="/about">
+        <NavLink
+          activeclassname="active"
+          className={nav__link}
+          to="/about"
+          onClick={updateMenu}
+        >
           About me
         </NavLink>
-        <NavLink activeclassname="active" className="nav-link" to="/portfolio">
+        <NavLink
+          activeclassname="active"
+          className={nav__link}
+          to="/portfolio"
+          onClick={updateMenu}
+        >
           PortFolio
         </NavLink>
       </div>

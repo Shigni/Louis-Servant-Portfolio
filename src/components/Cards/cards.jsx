7b2aaad24f49f2ba00648572fb1f49127ed0f067
shigni <louisservant86@gmail.com>
2023-06-18 11:40:32 +0200

    alt text on preview

--- src/components/Cards/cards.jsx
+++ src/components/Cards/cards.jsx
@@ -2,7 +2,7 @@ export function Card({ cover, preview, subtitle, alt_title }) {
   return (
     <article className="card-project">
       <img className="thumbnail" src={cover} alt={alt_title + " thumbnail"} />
-      <img className="preview" src={preview} alt={alt_title} />
+      <img className="preview" src={preview} alt={alt_title + " preview"} />
       <div className="card-project__layer">
         <div className="title-padding">
           <p className="card-project__title">{subtitle}</p>

2ccc9ad2e9fe141a22fa8b57aeab8e616c37f26a
shigni <louisservant86@gmail.com>
2023-06-20 11:29:24 +0200

    Better imports

---
+++ src/data/index.js
@@ -0,0 +1,4 @@
+export * as aboutMe_en from "./en/aboutMe.json";
+export * as aboutMe_fr from "./fr/aboutMe.json";
+export * as projets_en from "./en/projets.json";
+export * as projets_fr from "./fr/projets.json";
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
-import aboutMe_en from "./data/en/aboutMe.json";
-import aboutMe_fr from "./data/fr/aboutMe.json";
-import projets_en from "./data/en/projets.json";
-import projets_fr from "./data/fr/projets.json";
 import i18next from "i18next";
 import { I18nextProvider } from "react-i18next";
+import App from "./App";
+import { aboutMe_en, aboutMe_fr, projets_en, projets_fr } from "./data";
 
 const lng = navigator.language;
+
 i18next.init({
   interpolation: { escapeValue: false },
   lng: lng,
+  fallbackLang: "fr",
   resources: {
     fr: {
       aboutMe: aboutMe_fr,
@@ -24,6 +22,7 @@ i18next.init({
     },
   },
 });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
